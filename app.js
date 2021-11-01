bda89f285221b1447bf22601f5258e7733050a19
amsadara <thiiba95@gmail.com>
2021-11-01 11:42:21 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// console.log('Hello World')
+
+IntArray= [0,1,2,3,4,5,6,7,8,9];
+
+for(iterator of IntArray){
+    console.log(iterator);
+}
+
+fruits = [
+    {
+        designation:'Apple',
+        prix:5
+    },
+    {
+        designation:'Banane',
+    },
+    {
+        designation:'Mangue',
+        prix:5,
+        couleur:'yellow',
+    },
+];
+
+for (fruit of fruits){
+    console.log(fruit);
+}
