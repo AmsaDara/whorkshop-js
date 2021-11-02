3d261ad8f8e1a617f1a599b5f9865da93e712d82
amsadara <thiiba95@gmail.com>
2021-11-02 10:25:31 +0000

    add for of iteration

--- git.notes.md
+++ git.notes.md
@@ -1,3 +1,5 @@
 git init ("initializé git")
 git add . ( . stands for current directory )
 git commit -m "message"
+git status
+git log
--- iteration.js
+++ iteration.js
@@ -2,5 +2,9 @@ cities = ["Dakar","Saint Louis","Thies","Tamba","Matam"]
 
 for (var i=0;i<cities.length;i++){
     console.log(cities[i]);
-    i++;
+    // i=i+1;
+}
+console.log('*------------------------------------*')
+for(city of cities){
+    console.log(city)
 }
